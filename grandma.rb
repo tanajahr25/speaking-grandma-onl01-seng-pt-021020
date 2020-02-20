1095f5813de86afd6f381e7875d66fe21924abb7
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-02-20 00:30:19 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
      'I LOVE YOU TOO PUMPKIN!'
-   elseif phrase != phrase.upcase
+   elsif phrase != phrase.upcase()
     'HUH?! SPEAK UP, SONNY!'
    else
     'NO, NOT SINCE 1938!'
--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,6 +1,6 @@
 describe '#speak_to_grandma' do
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
-    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq ('I LOVE YOU TOO PUMPKIN!')
   end
 
   it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
